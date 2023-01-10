26733fa6a5fce49756c1df996b8699b4d63bf428
vinramalho <vinramalho10@gmail.com>
2023-01-10 02:06:27 -0300

    quarta pagina

--- src/pages/Crud/Crud.jsx
+++ src/pages/Crud/Crud.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Col, Row, Card, Skeleton, Avatar, Button, Modal, Input } from "antd";
 import {
   EditOutlined,
@@ -8,12 +9,22 @@ import {
   ExclamationCircleFilled,
 } from "@ant-design/icons";
 import MenuHeader from "../../components/MenuHeader";
+import ApiCrud from "../../services/ApiCrud";
 const { confirm } = Modal;
 
 const Crud = () => {
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [title, setTitle] = useState(null);
+  const [values, setValues] = useState();
+  const [data, setData] = useState([]);
+
+  const handleSetValues = (value) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  };
 
   const showDeleteConfirm = () => {
     confirm({
@@ -32,6 +43,33 @@ const Crud = () => {
     });
   };
 
+  const requestFetch = () => {
+    axios
+      .post("http://localhost:8000/api/create", values)
+      .then((res) => {
+        console.log(res);
+        fetchApi();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const fetchApi = async () => {
+    setLoading(true);
+    try {
+      const response = await ApiCrud.get("/read");
+      const userData = response.data;
+      setData(userData.reverse());
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return (
     <>
       <Row>
@@ -52,72 +90,111 @@ const Crud = () => {
               }}
             />
 
-            <Card
-              className="card"
-              key={""}
-              style={{ width: 550, marginTop: 16 }}
-              actions={[
-                <EditOutlined
-                  key="edit"
-                  onClick={() => {
-                    setOpen(true);
-                    setTitle("Editar usuario");
-                  }}
-                />,
-                <DeleteOutlined key="delete" onClick={showDeleteConfirm} />,
-              ]}
-            >
-              <Skeleton
-                loading={false}
-                avatar
-                active
-                paragraph={{
-                  rows: 3,
-                }}
-              >
-                <div className="avatar-group">
-                  <Avatar shape="circle" icon={<UserOutlined />} />
-                  <p>
-                    <b>USERNAME</b>
-                  </p>
-                </div>
+            {data.map((user, i) => {
+              // console.log(user);
+              return (
+                <>
+                  <Card
+                    className="card"
+                    key={i}
+                    style={{ width: 550, marginTop: 16 }}
+                    actions={[
+                      <EditOutlined
+                        key="edit"
+                        onClick={() => {
+                          setOpen(true);
+                          setTitle("Editar usuario");
+                        }}
+                      />,
+                      <DeleteOutlined
+                        key="delete"
+                        onClick={showDeleteConfirm}
+                      />,
+                    ]}
+                  >
+                    <Skeleton
+                      loading={loading}
+                      avatar
+                      active
+                      paragraph={{
+                        rows: 3,
+                      }}
+                    >
+                      <div className="avatar-group">
+                        <Avatar shape="circle" icon={<UserOutlined />} />
+                        <p>
+                          <b>{user.name}</b>
+                        </p>
+                      </div>
 
-                <p>Nome: nome sobrenome</p>
-                <p> Email: email</p>
-                <p>Cidade: cidade</p>
-                <p>Idade: idade</p>
-              </Skeleton>
-            </Card>
+                      <p>Email: {user.email}</p>
+                      <p>Telefone: {user.phone}</p>
+                      <p>Endereço: {user.address}</p>
+                      <p>CPF: {user.cpf}</p>
+                    </Skeleton>
+                  </Card>
+                </>
+              );
+            })}
 
             <Modal
               title={title}
               centered
               open={open}
-              onOk={() => setOpen(false)}
+              onOk={() => {
+                setOpen(false);
+                requestFetch();
+              }}
               onCancel={() => setOpen(false)}
               width={1000}
+              okText="Enviar"
               style={{
                 top: 20,
               }}
             >
               <Input
-                size="large"
-                placeholder="Username"
-                prefix={<UserOutlined />}
-              />
-              <br />
-              <br />
-              <Input
+                name="name"
                 size="large"
                 placeholder="Nome"
                 prefix={<UserOutlined />}
+                onChange={handleSetValues}
               />
               <br />
               <br />
               <Input
+                name="email"
                 size="large"
                 placeholder="Email"
                 prefix={<UserOutlined />}
+                onChange={handleSetValues}
+              />
+              <br />
+              <br />
+              <Input
+                name="phone"
+                size="large"
+                placeholder="Telefone"
+                prefix={<UserOutlined />}
+                onChange={handleSetValues}
+              />
+              <br />
+              <br />
+              <Input
+                name="address"
+                size="large"
+                placeholder="Endereço"
+                prefix={<UserOutlined />}
+                onChange={handleSetValues}
+              />
+              <br />
+              <br />
+
+              <Input
+                size="large"
+                placeholder="CPF"
+                name="cpf"
+                prefix={<UserOutlined />}
+                onChange={handleSetValues}
               />
             </Modal>
           </div>
